8930f1d7afee76e8a3049bf6ea0d78296c799f4f
Justin Fitzsimmons <justin@fitzsimmons.ca>
2019-02-06 23:40:19 -0500

    #5 - Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eo pipefail
+
+ENVIRONMENT_NAME=$1
+if [[ -z "$ENVIRONMENT_NAME" ]]; then
+  echo "Environment Name must be passed as the first argument"
+  exit 2
+fi
+
+./build.sh
+
+(
+	cd infrastructure
+	terraform workspace select "${ENVIRONMENT_NAME}"
+	terraform apply -var-file "config/${ENVIRONMENT_NAME}.tfvars.json"
+)
